f70967e1ae3bf10c7688bad337cbde08cdbd1f90
Stephan Kulla <git.mail@kulla.me>
2023-07-15 21:13:42 +0200

    feat: Add help() function

--- configure-repositories.py
+++ configure-repositories.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python
 
+import sys
+
+def main():
+    args = sys.argv[1:]
+
+    if len(args) == 0 or any(help_opt in args for help_opt in ["--help", "-h", "-?"]):
+        help()
+
+def help():
+    print_error("Usage: ./configure-repositories.py <path1> [<path2>...]")
+
+def print_error(message):
+    print(message, file=sys.stderr)
+
 if __name__ == "__main__":
-    print("Hello")
+    main()
