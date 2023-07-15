dbf64d76044cbeeec5950a04a4e707fa0c0d94cc
Stephan Kulla <git.mail@kulla.me>
2023-07-15 21:20:41 +0200

    Format configure-repositories.py with "black"

--- configure-repositories.py
+++ configure-repositories.py
@@ -2,17 +2,23 @@
 
 import sys
 
+
 def main():
     args = sys.argv[1:]
 
-    if len(args) == 0 or any(help_opt in args for help_opt in ["--help", "-h", "-?"]):
+    if len(args) == 0 or any(
+        help_opt in args for help_opt in ["--help", "-h", "-?"]
+    ):
         help()
+
 
 def help():
     print_error("Usage: ./configure-repositories.py <path1> [<path2>...]")
 
+
 def print_error(message):
     print(message, file=sys.stderr)
+
 
 if __name__ == "__main__":
     main()
