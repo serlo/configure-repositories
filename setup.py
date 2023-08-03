2679132128f419a8e74d094389e27fe0b0806f2a
eliflores <eli@serlo.org>
2023-08-03 17:04:41 +0200

    Add setup.py
    To set up the distribution of the CLI.

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='configure-repositories',
+    version='0.1.0',
+    description="A command line interface to configure repositories of the Serlo organization",
+    py_modules=['configure-repositories'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: CC0 1.0 Universal",
+    ],
+    install_requires=[
+        'click',
+    ],
+    entry_points='''
+        [console_scripts]
+        configure_repositories=configure_repositories:cli
+    ''',
+)
