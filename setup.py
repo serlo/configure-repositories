d09b7e6ed383caa7983742b0409dc2ff6f1d1e86
eliflores <eli@serlo.org>
2023-08-28 13:18:37 +0200

    Release version 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="configure-repositories",
-    version="0.1.0",
+    version="0.1.2",
     description="A command line interface to configure repositories of the Serlo organization",
     py_modules=["configure-repositories"],
     classifiers=[
