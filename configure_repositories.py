b1a6ef605a6a66ad5b7a5f13394694b3d3c67508
Andreas Huber <AndreasHuber@users.noreply.github.com>
2024-01-16 10:50:57 +0100

    refactor(mysql import anonymous data): make Python script configure using the  bash command instead of Typescript

--- configure_repositories.py
+++ configure_repositories.py
@@ -73,7 +73,7 @@ def setup_local_mysql_database(repo):
         package_json["scripts"].update(
             {
                 "mysql": "docker compose exec mysql serlo-mysql",
-                "mysql:import-anonymous-data": f"{ts_node_cmd} scripts/mysql/mysql-import-anonymous-data",
+                "mysql:import-anonymous-data": f"./scripts/mysql/mysql-import-anonymous-data.sh",
                 "mysql:rollback": f"docker compose exec {mysql_entrypoint_cmd}",
                 "start:docker": "docker compose up --detach",
                 "stop:docker": "docker compose down",
